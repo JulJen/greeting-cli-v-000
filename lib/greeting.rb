cbeb34b4392bc4a23aad9549d8f6500fd2359d9d
Julia Jen-Prog <juliajen.prog@gmail.com>
2017-10-03 00:14:14 +0000

    automatically backed up by learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,16 @@
+
 # code the #greeting method here!
+
+
+# puts "Hi! Welcome to the wonderful world of Ruby programming."
+# puts "Please enter your name so that we can greet you more personally:"
+# name = gets.strip
+
+# answer = "Hello name. It's nice to meet you."
+# greeting_method(anwser)
+
+def greeting(name)
+  puts "Hello #{name}! It's nice to meet you."
+end
+
+greeting(name)
